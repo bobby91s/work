59ee4894dae207499132d9b9d498ebab5860b436
bobby91s <wow.bogdansilaghi@gmail.com>
2018-07-05 11:07:28 +0300

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,26 @@
+import os
+
+"""
+Put this code in a .py file in the folder where your html-s are. This app will
+crawl through all the files in it's current directory and will search for a '.html' file.
+Insert in the variable called 'old' the string you want to replace and in the 'new' variable
+the string you want it replaced with. After you run the program it will replace anything
+from html tags to text in the html.
+You will also get a small feedback after it's done.
+"""
+
+old = ''
+new = ''
+
+
+for filename in os.listdir(os.getcwd()):
+    if '.html' in filename:
+        with open(filename) as f:
+            newText=f.read().replace(old, new)
+
+        with open(filename, "w") as f:
+            f.write(newText)
+
+
+
+print ('Done')
